6acc38bf9338f2253889e4c2a0be74a5d4fd6c42
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-30 15:58:58 +0900

    Create security-groups.tf

---
+++ security-groups.tf
@@ -0,0 +1,34 @@
+# modify default security groups
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.k8s_vpc.id
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
