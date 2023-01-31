f47ebe0b5dc7f6b6f7eb78f5d41476f2f25e0a4a
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-31 17:52:23 +0900

    Update k8s_worker_setup.sh

--- k8s_setup/k8s_worker_setup.sh
+++ k8s_setup/k8s_worker_setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo kubeadm join --token 777777.7777777777777777 \
-             --discovery-token-unsafe-skip-ca-verification < master-node ip >:6443
+             --discovery-token-unsafe-skip-ca-verification $1:6443
