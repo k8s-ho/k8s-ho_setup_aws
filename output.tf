85f73e61d58d54a30ea129d7a5d92b8b5d37be94
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-02-01 10:52:26 +0900

    Update output.tf
    fin

--- output.tf
+++ output.tf
@@ -5,14 +5,12 @@ output "master-k8sHo_private_ip" {
     value = aws_instance.master.private_ip
 }
 
-
 output "worker-1-k8sHo_public_ip" {
     value = aws_instance.worker-1.public_ip
 }
 output "worker-1-k8sHo_private_ip" {
     value = aws_instance.worker-1.private_ip
 }
-
 
 output "worker-2-k8sHo_public_ip" {
     value = aws_instance.worker-2.public_ip
@@ -21,11 +19,9 @@ output "worker-2-k8sHo_private_ip" {
     value = aws_instance.worker-2.private_ip
 }
 
-
 output "worker-3-k8sHo_public_ip" {
     value = aws_instance.worker-3.public_ip
 }
 output "worker-3-k8sHo_private_ip" {
     value = aws_instance.worker-3.private_ip
 }
-
