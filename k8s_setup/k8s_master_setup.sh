d8f88e903bd72d0a4995223baca4a0d0d6323252
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-02-13 13:00:28 +0900

    Update k8s_master_setup.sh

--- k8s_setup/k8s_master_setup.sh
+++ k8s_setup/k8s_master_setup.sh
@@ -15,7 +15,9 @@ echo "alias k=kubectl" | sudo tee -a $HOME/.bashrc
 echo "complete -o default -F __start_kubectl k" | sudo tee -a $HOME/.bashrc
 source $HOME/.bashrc 
 
-kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+sudo wget https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+sudo sed -i -e 's?10.244.0.0/16?172.16.0.0/16?g' kube-flannel.yml 
+kubectl apply -f kube-flannel.yml
 
 #sudo curl -O https://projectcalico.docs.tigera.io/manifests/calico.yaml 
 #sudo sed -i -e 's?192.168.0.0/16?172.16.0.0/16?g' calico.yaml
